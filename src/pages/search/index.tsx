1d9f3fc25c150b70166c86ef2bc4f7baa6245c07
woojo230 <woojunghyck@naver.com>
2025-03-02 22:16:02 +0900

    검색창 ssr 구현

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,13 +1,31 @@
 import SearchableLayout from '@/components/searchable-layout';
 import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch.books';
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  // console.log(context);
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+
+  return {
+    props: {
+      books,
+    },
+  };
+};
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   // const router = useRouter(); // 쿼리 파라미터 -> 쿼리 스트링 가져올 수 있음
   // const queryString = router.query.q;
   // console.log(queryString);
+  console.log(books);
 
   return (
     <div>
