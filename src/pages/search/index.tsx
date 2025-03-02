08bef992b0839c2210d4297f1a1b7237e1d656da
woojo230 <woojunghyck@naver.com>
2025-03-02 17:44:19 +0900

    ui 구현

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,13 +1,21 @@
 import SearchableLayout from '@/components/searchable-layout';
 import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
+import books from '@/mock/books.json';
+import BookItem from '@/components/book-item';
 
 export default function Page() {
-  const router = useRouter(); // 쿼리 파라미터 -> 쿼리 스트링 가져올 수 있음
-  const queryString = router.query.q;
-  console.log(queryString);
+  // const router = useRouter(); // 쿼리 파라미터 -> 쿼리 스트링 가져올 수 있음
+  // const queryString = router.query.q;
+  // console.log(queryString);
 
-  return <h1>page{queryString}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
